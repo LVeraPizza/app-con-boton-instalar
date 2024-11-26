3380e2a02b18e2acf5f56d20750cb0ebd4ca0ccc
LVeraPizza <veinticinco95@hotmail.com>
2024-11-26 15:11:40 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/contacto.html',
+        '/cc/styles.css',
+        '/script.js',
+        '/splash.js',
+        '/service-worker.js',
+
+        // Agrega otros archivos que necesites en el caché
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
